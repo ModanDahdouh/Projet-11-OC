f417a056080c0548ef25072e1350da045c043f84
ModanDahdouh <mrdahdouhmodan@gmail.com>
2024-02-18 03:45:02 +0100

    Creation des Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Signin from "./pages/Signin/Signin";
+import User from "./pages/User/User";
+import Error from "./pages/Error/Error";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/user" element={<User />} />
+            <Route path="*" element={<Error />} />
+        </Routes>
+    );
 }
 
 export default App;
